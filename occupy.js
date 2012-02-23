74bccb79ddfc9e59a5031c27b049854aaac7ee73
Luiz Lopes <luizlopes@gmail.com>
2012-02-23 15:19:02 -0500

    bumped up version \n lint occupy.js

--- occupy.js
+++ occupy.js
@@ -1,18 +1,19 @@
-(function() {
+(function () {
+ "use strict";
  var io, Conduit;
 
  io = require('socket.io').listen(1400); 
  Conduit = require('flickr-conduit').Conduit;
 
- process.on('uncaughtException', function(){});
+ process.on('uncaughtException', function (){});
 
  var globalEvent = 'occupy';
 
  var conduit = new Conduit();
- conduit.subscribeCallback = function(urlParts) {
-    return urlParts.query.verify_token == 'myspecialcallbacktoken';
+ conduit.subscribeCallback = function (urlParts) {
+    return urlParts.query.verify_token === 'myspecialcallbacktoken';
  };
- conduit.getEventName = function() {
+ conduit.getEventName = function () {
     return globalEvent;
  };
  conduit.listen(1401);
@@ -20,14 +21,16 @@
  var buffer = [];
  var seen = {};
 
- io.sockets.on('connection', function(socket) {
+ io.sockets.on('connection', function (socket) {
 
     // Give them something fun to look at...
     for (var i in buffer) {
-        socket.emit('publish', buffer[i]); 
+        if (buffer.hasOwnProperty(i)) {
+            socket.emit('publish', buffer[i]);
+        }
     }
 
-    conduit.on(globalEvent, function(img) {
+    conduit.on(globalEvent, function (img) {
 
         if (!seen[img.url]) {
             if (buffer.length >= 15) {
@@ -44,7 +47,7 @@
  });
 
 
- setInterval(function() {
+ setInterval(function () {
     console.log('Number of clients: ' + io.sockets.clients().length); 
     conduit.heartbeat(globalEvent);
  }, 10000);
